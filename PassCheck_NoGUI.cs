20ff66f0ea76071ff5d6534a73e6a22299672817
Ofelia888 <acbk123@gmail.com>
2024-10-24 12:16:40 +0200

    PasswordCheck

---
+++ PassCheck_NoGUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PW_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Password must contain: 8-20 characters, upper case letters, lower case letters, numbers and min. 1 special character");
+                Console.WriteLine("Type password below: ");
+                string pass = Console.ReadLine();
+                bool has_specialChars = false;
+
+                foreach (char c in pass)
+                {
+                    if (char.IsLetter(c) || char.IsDigit(c))
+                        continue;
+                    has_specialChars = true;
+                    break;
+                }
+
+
+                if (pass.Length >= 8 && pass.Length < 20 && pass.Any(char.IsUpper) && pass.Any(char.IsLower) && pass.Any(char.IsDigit) && has_specialChars)
+                {
+                    Console.WriteLine("Password accepted");
+                    break;
+                }
+                else
+                    Console.WriteLine("Password does not meet requirements");
+            }
+        }
+    }
+}
+
+
+    
